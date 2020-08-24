f02c5eef9aa851ea848c61da1ca85c73a8ec5288
Elior Dayari <307902254@STUD.colman.ac.il>
2020-08-24 14:37:12 +0300

    commit it!

---
+++ Pro_Sb/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pro_Sb.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string lastName { get; set; }
+        public int  Grade { get; set; }
+
+    }
+}
